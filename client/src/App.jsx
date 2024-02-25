989f62d80653eb85646e427cd00c2e8dc09a43d0
crispynoodlesoup <thepanman321@gmail.com>
2024-02-24 20:49:25 -0600

    style app a little

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,14 @@
-import { useState } from 'react'
+import appStyle from "./app.module.css"
 
 function App() {
   return (
     <>
-      
+    <nav></nav>
+    <main>
+      <div className={appStyle.content}>
+
+      </div>
+    </main>
     </>
   )
 }
